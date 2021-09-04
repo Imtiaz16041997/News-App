9c69c9f99c8e19ed4d2641374106cf4e1bc92c67
imtiaz1997 <imtiazeemel42@gmail.com>
2021-09-04 20:53:40 +0600

    Structure set

---
+++ app/src/main/java/com/example/newsapp/Fragment/TechnologyFragment.java
@@ -0,0 +1,25 @@
+package com.example.newsapp.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.newsapp.R;
+
+
+public class TechnologyFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_technology, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+
+import com.example.newsapp.Adapter.PagerAdapter;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabHome,tabScience,tabHealth, tabTechnology,tabEntertainment,tabSports;
+    PagerAdapter pagerAdapter;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
